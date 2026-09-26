1fedd048ee47cdb2a7eb2415bc4782e11ecd4989
Gopal Yadav <gopal.x2357@gmail.com>
2026-09-27 00:00:11 +0530

    pass by value and reference

---
+++ CreateChangeFxn.cpp
@@ -0,0 +1,31 @@
+//pass by value and reference
+#include<iostream>
+using namespace std;
+
+class Book {
+    public:
+    string title;
+    string author;
+    int pages;
+    int price;
+};
+
+void printBookDetails(Book b) {
+    cout << "Title: " << b.title << " " << "Author: " << b.author << " " << "Pages: " << b.pages << " " << "Price: " << b.price << endl;
+}
+void Change(Book b){
+    b.author ="GATE 2027"; // not changes  because of pass by value 
+    b.pages =21000;
+}
+
+int main() {
+    Book b1;
+    b1.title = "C++ Programming";
+    b1.author = "Bjarne Stroustrup";
+    b1.pages = 500;
+    b1.price = 1500;
+    printBookDetails(b1);
+    Change(b1);
+    printBookDetails(b1);
+    return 0;
+}
